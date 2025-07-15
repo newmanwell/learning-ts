26b25a14ff3f695faf1d1ed6772cb72c6272d302
Shaun Newman <shaun.newman.p@gmail.com>
2025-07-15 15:08:08 -0400

    added method to class to show how private can be accessed inside the class

--- lesson6/src/main.ts
+++ lesson6/src/main.ts
@@ -1,4 +1,8 @@
 class Developer {
+
+  // ! assertion allows you to initalize and makes TS happy
+  whatsGoingOn!: string;
+
   name: string;
   computer: string;
   age: number;
@@ -13,11 +17,22 @@ class Developer {
 }
 
 // Visibility Modifiers
-class SomeGuy {
-  constructor(public readonly name: string, public age: number, private isTall: boolean, protected city: string) {
+class SomeGuy {                                                                                         // has default value
+  constructor(public readonly name: string, public age: number, protected city: string, private isTall: boolean = false) {
     this.name = name;
     this.age = age;
-    this.isTall = isTall;
     this.city = city;
+    this.isTall = isTall;
+  }
+
+  // Method: Can use isTall even though it is private becuse it is still inside the class
+  public getTallness() {
+    return `Am I tall? ${this.isTall}!`
   }
 }
+
+const Shaun = new SomeGuy('Shaun', 47, 'Jacksonville', true);
+const Jim = new SomeGuy('Jim', 68, 'London'); // used default value for isTall
+
+console.log(Shaun.getTallness);
+// console.log(Shaun.isTall); nope, isTall is private
