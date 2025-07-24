9f0ecdbac6cd132a527c020979fa4875d1ebe137
Shaun Newman <shaun.newman.p@gmail.com>
2025-07-24 16:33:08 -0400

    added function for required

--- lesson9/build/js/main.js
+++ lesson9/build/js/main.js
@@ -10,3 +10,11 @@ const assign0 = {
 };
 console.log(updateAssignment(assign0, { grade: 88 }));
 const assignGraded = updateAssignment(assign0, { grade: 88 });
+// Required and Readonly
+const recordAssignment = (assign) => {
+    return assign;
+};
+const assignVerified = Object.assign(Object.assign({}, assignGraded), { verified: false });
+// assignVerified.title = "Lesson 69" not allowed because assignVerified is readonly
+// recordAssignment(assignGraded) not allowed because recordAssignment is required for all properties and assignGraded only has grade
+recordAssignment(Object.assign(Object.assign({}, assignGraded), { verified: false }));
--- lesson9/src/main.ts
+++ lesson9/src/main.ts
@@ -21,3 +21,15 @@ const assign0: Assignment = {
 
 console.log(updateAssignment(assign0, {grade: 88}));
 const assignGraded: Assignment = updateAssignment(assign0, {grade: 88});
+
+// Required and Readonly
+
+const recordAssignment = (assign: Required<Assignment>): Assignment => {
+  return assign;
+}
+
+const assignVerified: Readonly<Assignment> = {...assignGraded, verified: false};
+
+// assignVerified.title = "Lesson 69" not allowed because assignVerified is readonly
+// recordAssignment(assignGraded) not allowed because recordAssignment is required for all properties and assignGraded only has grade
+recordAssignment({...assignGraded, verified: false});
