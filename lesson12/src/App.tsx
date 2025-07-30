fec4badf9960bee00680f392edc8823dc9b6e6bc
Shaun Newman <shaun.newman.p@gmail.com>
2025-07-30 16:10:58 -0400

    feat: added Counter component and passed setCount and children props

--- lesson12/src/App.tsx
+++ lesson12/src/App.tsx
@@ -1,14 +1,19 @@
-import Heading from "./components/Heading"
-import { Section } from "./components/Section"
+import Heading from "./components/Heading";
+import { Section } from "./components/Section";
+import Counter from "./components/Counter";
+
+import { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState<number>(1)
 
   return (
     <>
       <Heading title={"Good Day Sir!"}/>
       <Section title={"Some Silly Prop"}>This is where children go!</Section>
+      <Counter setCount={setCount}> Current Number: {count} </Counter>
     </>
   )
 }
 
-export default App
+export default App;
